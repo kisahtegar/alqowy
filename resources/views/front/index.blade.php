b08ec3c008bc1521423928cee60df6ca55a75d2f
Kisah Tegar Putra Abdi <code.kisahtegar@gmail.com>
2024-08-26 17:28:07 +0700

    Adding routing project.

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,1 @@
+ini adalahan halaman depan
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseVideoController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscribeTransactionController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// User without login
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/details/{course:slug}', [FrontController::class, 'details'])->name('front.details');
+Route::get('/category/{course:slug}', [FrontController::class, 'category'])->name('front.category');
+Route::get('/pricing', [FrontController::class, 'pricing'])->name('front.pricing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +23,28 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Need to logged in before create a transaction
+    Route::get('/checkout', [FrontController::class, 'checkout'])->name('front.checkout');
+    Route::get('/checkout/store', [FrontController::class, 'checkout_store'])->name('front.checkout.store');
+
+    // For admin section
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('categories', CategoryController::class)
+        ->middleware('role:owner'); // admin.categories.index
+
+        Route::resource('teachers', TeacherController::class)
+        ->middleware('role:owner');
+
+        Route::resource('courses', CourseController::class)
+        ->middleware('role:owner|teacher');
+
+        Route::resource('subscribe_transactions', SubscribeTransactionController::class)
+        ->middleware('role:owner');
+
+        Route::resource('course_videos', CourseVideoController::class)
+        ->middleware('role:owner|teacher');
+    });
 });
 
 require __DIR__.'/auth.php';
