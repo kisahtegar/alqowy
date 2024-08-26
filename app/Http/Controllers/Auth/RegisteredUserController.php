3c8b971b9c899f822ea7f258966d49e611fd4392
Kisah Tegar Putra Abdi <code.kisahtegar@gmail.com>
2024-08-26 13:09:27 +0700

    Adding models, migration, controller and attributes in tables.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,24 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'occupation' => ['required', 'string', 'max:255'],
+            'avatar' => ['required', 'image', 'mimes:png,jpg,jpeg'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        // Store the uploaded avatar file if available, otherwise use a default image path.
+        if ($request->hasFile('avatar')) {
+            $avatarPath = $request->file('avatar')->store('avatars', 'public');
+        } else {
+            $avatarPath = 'images/avatar-default.png';
+        }
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'occupation' => $request->occupation,
+            'avatar' => $request->avatarPath,
             'password' => Hash::make($request->password),
         ]);
 
