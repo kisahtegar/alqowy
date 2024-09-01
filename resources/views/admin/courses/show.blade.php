9a9aa809afe7b3dd1cbf2243ea0e3fd95462379c
Kisah Tegar Putra Abdi <code.kisahtegar@gmail.com>
2024-09-01 23:49:42 +0700

    Implementing feature teacher add, delete, and edit course.

--- resources/views/admin/courses/show.blade.php
+++ resources/views/admin/courses/show.blade.php
@@ -12,21 +12,21 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
                 <div class="item-card flex flex-row gap-y-10 justify-between items-center">
                     <div class="flex flex-row items-center gap-x-3">
-                        <img src="https://images.unsplash.com/photo-1552196563-55cd4e45efb3?q=80&w=3426&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="rounded-2xl object-cover w-[200px] h-[150px]">
+                        <img src="{{Storage::url($course->thumbnail)}}" alt="" class="rounded-2xl object-cover w-[200px] h-[150px]">
                         <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">Diet For Beginners in 2024</h3>
-                            <p class="text-slate-500 text-sm">Cardio</p>
+                            <h3 class="text-indigo-950 text-xl font-bold">{{$course->name}}</h3>
+                            <p class="text-slate-500 text-sm">{{$course->category->name}}</p>
                         </div>
                     </div>
                     <div class="flex flex-col">
                         <p class="text-slate-500 text-sm">Students</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">183409</h3>
+                        <h3 class="text-indigo-950 text-xl font-bold">{{$course->students->count()}}</h3>
                     </div>
                     <div class="flex flex-row items-center gap-x-3">
-                        <a href="#" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                        <a href="{{route('admin.courses.edit', $course)}}" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                             Edit Course
                         </a>
-                        <form action="#" method="POST">
+                        <form action="{{route('admin.courses.destroy', $course)}}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
@@ -41,7 +41,7 @@
                 <div class="flex flex-row justify-between items-center">
                     <div class="flex flex-col">
                         <h3 class="text-indigo-950 text-xl font-bold">Course Videos</h3>
-                        <p class="text-slate-500 text-sm">1893 Total Videos</p>
+                        <p class="text-slate-500 text-sm">{{$course->course_videos()->count()}} Total Videos</p>
                     </div>
                     <a href="#" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                         Add New Video
